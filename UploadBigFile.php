8a524728c2ff22f601922546f4824b38e5ed5b06
xiabeifeng <840679824@qq.com>
2016-05-02 00:27:06 +0800

    modify

--- UploadBigFile.php
+++ UploadBigFile.php
@@ -1,10 +1,18 @@
 <?php
-if (Request::get('act') == 'upload') {
-    $name = Request::post('name');
-    $total = Request::post('total');
-    $index = Request::post('index');
-    $data = $_FILES['data'];
-    var_dump($_POST);
-    var_dump($_FILES);
+if ($_GET['act'] == 'upload') {
+    $index = $_POST['index'];
+    $filename = "./ups/$index.mp4";
+    $result = move_uploaded_file($_FILES['data']['tmp_name'], $filename);
+    if ($result) {
+        echo json_encode(array('errno'=>200, 'message'=>'ok'));
+    } else {
+        echo json_encode(array('errno'=>1001, 'message'=>'上传失败'));
+    }
+} elseif ($_GET['act'] == 'join') {
+    $total = intval($_POST['total']);
+    for($i = 1; $i<=$total; $i++){
+        file_put_contents('./ups/new.mp4', file_get_contents("./ups/$i.mp4"), FILE_APPEND);
+        @unlink("./ups/$i.mp4");
+    }
+    echo json_encode(array('errno'=>200, 'message'=>'上传成功'));    
 }
-
